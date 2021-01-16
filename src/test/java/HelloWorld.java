14e41e4d8e1e065e054f01c0bfc744744267afae
a m <adibm@macbook-air.home>
2021-01-16 09:47:11 -0800

    xpaths added with driver commands

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -5,14 +6,18 @@ import java.util.concurrent.TimeUnit;
 
 public class HelloWorld {
     public static void main (String [] args){
+        String xpathforinput= "//input[@type='text']";
+        String xpathforclick= "//a[@class='searchButton]";
+
 
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/test/resources/chromedriver");
         WebDriver driver= new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         driver.get("https://darksky.net/");
-
-
+        driver.findElement(By.xpath(xpathforinput)).clear();
+        driver.findElement(By.xpath(xpathforinput)).sendKeys("new york");
+        driver.findElement(By.xpath(xpathforclick)).click();
 
     }
 }
